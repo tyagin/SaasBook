9ba37cfa26d5261aa3a187c3203ce3637eecb360
Bhawna <bhawna@bhawna-VirtualBox.(none)>
2014-02-13 02:33:36 -0500

    This is all removal

--- Demoapp/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '3b18dad42b35595434937f9e5cee2ffdbe1b69c9adef5a65d255614a905fe328eccace954a62f665cd53c2f9116baab2f098b6aa3a3413808a551b2d044ff759';
